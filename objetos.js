2297e05fc17fcf4c33bca5ec705969c779e55780
oliveiraduda123 <eduarda.sousa@estudante.ifro.edu.br>
2024-03-21 10:37:09 -0400

    commit

--- objetos.js
+++ objetos.js
@@ -1,11 +1,3 @@
-/*
-01. Crie a variável apartamento e atribua a ela um objeto literal com as seguintes propriedades:
-    quartos = 2
-    tipo = “apartamento”
-    endereco = “Avenida Principal, 456 - Centro”
-    andar: 7
-    Em seguida, exiba no console a seguinte frase, utilizando todas as propriedades da variável casa: "Apartamento com 2 quartos, localizado no 7º andar da Av. Principal, 456 - Centro.".
-*/
 let apartamento = {
     quartos: 2,
     tipo: 'Apartamento',
@@ -14,15 +6,6 @@ let apartamento = {
 }
 console.log(`${apartamento.tipo} com ${apartamento.quartos} quartos, localizado no ${apartamento.andar}º andar da ${apartamento.endereco}.`)
 
-/*
-02. Imagine que você está desenvolvendo um sistema para gerenciar produtos em um armazém. Crie uma variável chamada produtoEmbalado que execute as seguintes operações:
-    Atribua um objeto literal à variável com as seguintes propriedades:
-    nome: "Laptop HP"
-    categoria: "Eletrônicos"
-    peso: 1.5
-    preco: 3500.00
-Exiba no console uma frase que utilize todas as propriedades do objeto, como o exemplo abaixo: "O produto Laptop HP, da categoria Eletrônicos, pesando 1.5 kg, está à venda por R$ 3500.00."
-*/
 let produtoEmbalado = {
     nome: 'Laptop HP',
     categoria: 'Eletrônicos',
@@ -31,23 +14,6 @@ let produtoEmbalado = {
 }
 console.log(`O produto ${produtoEmbalado.nome}, da categoria ${produtoEmbalado.categoria}, pesando ${produtoEmbalado.peso} kg, está à venda por ${produtoEmbalado.preco.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}.`);
 
-/*
-03. Crie uma classe chamada Imovel com os seguintes atributos:
-    quartos: 
-    tipo: 
-    endereco: 
-    Crie um método na classe chamado exibirInformacoes que retorna uma string com as informações do imóvel.
-    Em seguida, instancie dois objetos da classe Imovel representando uma casa e um apartamento com as seguintes características:
-    Casa:
-    Quartos: 4
-    Tipo: "Casa"
-    Endereço: "Rua da Amizade, 789 - Bairro Alegre"
-    Apartamento:
-    Quartos: 2
-    Tipo: "Apartamento"
-    Endereço: "Avenida da Paz, 123 - Centro"
-    Chame o método exibirInformacoes para cada instância e exiba o resultado no console.
-*/
 class Imovel {
     constructor(quartos, tipo, endereco) {
         this.quartos = quartos;
@@ -66,23 +32,6 @@ console.log(casa.exibirInformacoes());
 const ap = new Imovel(2, "Apartamento", "Avenida da Paz, 123 - Centro");
 console.log(ap.exibirInformacoes());
 
-/*
-04. Crie uma classe chamada Veiculo com os seguintes atributos:
-    marca
-    modelo
-    ano
-    Crie um método na classe chamado exibirDetalhes que retorna uma string com as informações do veículo.
-    Em seguida, instancie dois objetos da classe Veiculo representando um carro e uma motocicleta com as seguintes características:
-    Carro:
-    Marca: "Toyota"
-    Modelo: "Corolla"
-    Ano: 2022
-    Motocicleta:
-    Marca: "Honda"
-    Modelo: "CBR 600RR"
-    Ano: 2021
-    Chame o método exibirDetalhes para cada instância e exiba o resultado no console.
-*/
 class Veiculo {
     constructor(marca, modelo, ano) {
         this.marca = marca;
--- operadores.js
+++ operadores.js
@@ -1,4 +1,3 @@
-//5.	Crie um programa que declare duas variáveis e exiba o resultado da soma, subtração, multiplicação e divisão desses números.
 let numero1 = 10;
 let numero2 = 5;
 
@@ -12,7 +11,6 @@ console.log("Subtração:", subtracao);
 console.log("Multiplicação:", multiplicacao);
 console.log("Divisão:", divisao);
 
-//6.	Declare duas idades e utilize operadores de comparação para verificar se uma pessoa é mais velha que a outra.
 let idade1 = 25;
 let idade2 = 30;
 
@@ -23,15 +21,11 @@ if (idade1 > idade2) {
 } else {
     console.log("As duas pessoas têm a mesma idade.");
 }
-
-//7.	Crie uma variável idade com um número e use o operador ternário para verificar se o valor armazenado na variável é maior ou igual a 18.
-//Imprima “Maior de idade” ou “Menor de idade”.
 let idade = 20;
 
 let statusMaioridade = (idade >= 18) ? "Maior de idade" : "Menor de idade";
 console.log(statusMaioridade);
 
-//8.	Crie uma variável com um número e use o operador ternário para verificar se é par ou ímpar.
 let numero = 7;
 
 let parOuImpar = (numero % 2 === 0) ? "Par" : "Ímpar";
