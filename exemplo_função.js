dc1915d761818e68c0138c1092452bffd563a7d8
oliveiraduda123 <eduarda.sousa@estudante.ifro.edu.br>
2024-03-28 10:42:08 -0400

    a

--- exemplo_função.js
+++ exemplo_função.js
@@ -2,3 +2,11 @@ var fatorial = function fac(n) {
     return n < 2 ? 1 : n * fac(n - 1);
 };
 console.log(fatorial(3));
+
+
+var minhafuncao;
+if (num == 0){
+    minhafuncao = function (objeto) {
+        objeto.make = "Toyota";
+    };
+}
